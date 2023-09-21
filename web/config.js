30e959387e6b7acde2044ef9c51f53ef845432ac
vcarrier <vincent.carrier@cegepmontpetit.ca>
2023-09-21 01:09:39 -0400

    modif initiale de config.js

--- web/config.js
+++ web/config.js
@@ -1,7 +1,7 @@
 const config = {
-    nom: "Code - Nom du cours",
-    description: "Site de référence du cours de x",
-    nomUrl: "depinfo-template"
+    nom: "420-B64-EM - Scriptage sous Windows ",
+    description: "Site de référence du cours de scriptage sous Windows (formation continue)",
+    nomUrl: "b64-scriptage-sous-windows"
 }
 
 module.exports = config;
